317d5b1e43ebdced55aa2d2dc8bc29707f596f0e
Robert Vojta <robert@balena.io>
2018-11-02 12:58:19 +0100

    Remove unused macro
    Signed-off-by: Robert Vojta <robert@balena.io>

--- tests/engine/eval_as_bool/relational.rs
+++ tests/engine/eval_as_bool/relational.rs
@@ -9,14 +9,6 @@ macro_rules! test_eval_eq {
     }};
 }
 
-macro_rules! test_eval_err {
-    ($e:expr) => {{
-        let engine = Engine::default();
-        let context = Context::default();
-        assert!(engine.eval_as_bool(&$e.parse().unwrap(), &context).is_err());
-    }};
-}
-
 #[test]
 fn test_logical_equal() {
     test_eval_eq!("true == true", true);
