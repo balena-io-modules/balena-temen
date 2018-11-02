18bab95c89569cd0325bbb31d6108f2054138179
Robert Vojta <robert@balena.io>
2018-11-02 08:03:54 +0100

    Add num parser errors
    Signed-off-by: Robert Vojta <robert@balena.io>

--- src/error.rs
+++ src/error.rs
@@ -8,5 +8,7 @@ error_chain! {
     foreign_links {
         Json(serde_json::Error) #[doc = "JSON (de)serialization error"];
         Pest(pest::error::Error<Rule>) #[doc = "Parser error"];
+        ParseIntError(std::num::ParseIntError) #[doc = "Integer parser error"];
+        ParseFloatError(std::num::ParseFloatError) #[doc = "Float parser error"];
     }
 }
