66325185b822281ea6026e6bd05f9f54bd7bf7f0
Cyryl Płotnicki <cyryl@balena.io>
2018-11-22 12:27:24 +0000

    Use `helper::eval` as the function to be exported to WASM
    Change-type: patch

--- src/wasm.rs
+++ src/wasm.rs
@@ -5,33 +5,18 @@ pub use console_error_panic_hook::set_once as set_panic_hook;
 use wasm_bindgen::prelude::JsValue;
 use wasm_bindgen::prelude::wasm_bindgen;
 
-use crate::ast::Identifier;
-use crate::Context;
-use crate::Engine;
-use crate::error::Error;
+use crate::eval;
 
+/// This re-exports `eval` into WASM-land, as `temen_evaluate`, with a prefix to avoid global namespace confusion
 #[wasm_bindgen]
-pub fn temen_evaluate(expression: JsValue, data: JsValue) -> Result<JsValue, JsValue> {
+pub fn temen_evaluate(data: JsValue) -> Result<JsValue, JsValue> {
     // use console.log for nice errors from Rust-land
     console_error_panic_hook::set_once();
 
-    let engine = Engine::default(); // Default functions, filters
-    let mut ctx = Context::default(); // Default context
-    let position = Identifier::default(); // Evaluate from the root
-
-    let expression = expression
-        .as_string()
-        .ok_or_else(|| Error::with_message("expression must be a string"))
-        .map_err(|e| JsValue::from(format!("{:#?}", e)))?;
-    let data = data
-        .as_string()
-        .ok_or_else(|| Error::with_message("data must be a string"))
-        .map_err(|e| JsValue::from(format!("{:#?}", e)))?;
-    let data = serde_json::from_str(&data).map_err(|e| JsValue::from(format!("{:#?}", e)))?;
-    engine
-        .eval(&expression, &position, &data, &mut ctx)
-        .map(|value| value.to_string().into())
-        .map_err(|e| format!("{:#?}", e).into())
+    let data = data.into_serde().map_err(|e| JsValue::from(format!("{:#?}", e)))?;
+    eval(data)
+        .map(|value| JsValue::from(value.to_string()))
+        .map_err(|e| JsValue::from(format!("{:#?}", e)))
 }
 
 #[cfg(test)]
@@ -39,16 +24,38 @@ mod tests {
     use wasm_bindgen_test::wasm_bindgen_test;
     use wasm_bindgen_test::wasm_bindgen_test_configure;
     use serde_json::json;
+    use pretty_assertions::assert_eq;
 
     use super::*;
 
     wasm_bindgen_test_configure!(run_in_browser);
 
     #[wasm_bindgen_test]
-    fn run_in_browser() {
-        let data = json!({});
-        let expression = "1 + 2";
-        let evaluated = temen_evaluate(expression.into(), data.to_string().into()).unwrap();
-        assert_eq!( JsValue::from("3"), evaluated);
+    fn run_as_wasm_in_browser() {
+        let data = JsValue::from_serde(&json!(
+        {
+            "number": 3,
+            "value": {
+                 "$$eval": "super.number + 5"
+            }
+        }))
+        .unwrap();
+
+        let evaluated = temen_evaluate(data).unwrap().as_string().unwrap();
+        // use same serde_json `.to_string` to format both, to not rely on
+        // JSValue formatter and serde_json formatter to be same
+        let evaluated: serde_json::Value = serde_json::from_str(&evaluated).unwrap();
+        let evaluated = evaluated.to_string();
+
+        let expected = json!(
+        {
+            "number": 3,
+            "value": 8
+        })
+        .to_string();
+
+        // equality operators on JsValues do not seem to produce expected results
+        // comparing string representations here instead
+        assert_eq!(expected, evaluated);
     }
 }
