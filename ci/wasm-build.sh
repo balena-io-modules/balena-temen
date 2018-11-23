666033efe4b0696f76a55bcd0380ab3327afff11
Robert Vojta <robert@balena.io>
2018-11-23 09:31:36 +0100

    Add comment why we do this
    Signed-off-by: Robert Vojta <robert@balena.io>

--- ci/wasm-build.sh
+++ ci/wasm-build.sh
@@ -25,6 +25,13 @@ wasm-pack build -t browser -d pkg
 PKG_NAME=$(jq -r .name pkg/package.json | sed 's/\-/_/g')
 
 # Merge nodejs & browser packages
+#
+# The reason for this is that wasm-pack doesn't support --target=all, but
+# just either --target nodejs or --target browser. We build these packages
+# separately and the merge them into one isomorphic package for both
+# environments.
+#
+# See https://github.com/rustwasm/wasm-pack/issues/313
 cp "pkg-node/${PKG_NAME}.js" "pkg/${PKG_NAME}_main.js"
 sed "s/require[\(]'\.\/${PKG_NAME}/require\('\.\/${PKG_NAME}_main/" "pkg-node/${PKG_NAME}_bg.js" > "pkg/${PKG_NAME}_bg.js"
 jq ".files += [\"${PKG_NAME}_bg.js\"]" pkg/package.json \
