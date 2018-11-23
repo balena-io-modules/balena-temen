0af8476777ba3b122857b3a4bb64afd48e10f524
Robert Vojta <robert@balena.io>
2018-11-23 08:44:36 +0100

    Fix npm-cli login command
    Signed-off-by: Robert Vojta <robert@balena.io>

--- ci/publish.sh
+++ ci/publish.sh
@@ -12,5 +12,5 @@ cargo publish --dry-run
 # repo.yml.type == rust-crate-wasm
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 "$DIR/wasm-build.sh"
-npm-cli login
+npm-cli-login
 npm publish --access public pkg --dry-run
