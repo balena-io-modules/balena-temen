8ed8afeab272e68ce820b8f78bdbcf9bb272d380
Robert Vojta <robert@balena.io>
2018-11-23 09:01:06 +0100

    Disable publishing
    Signed-off-by: Robert Vojta <robert@balena.io>

--- ci/publish.sh
+++ ci/publish.sh
@@ -7,10 +7,13 @@ source $HOME/.cargo/env
 
 # repo.yml.type == rust-* (rust-crate, rust-crate-wasm)
 cargo login "$CARGO_API_TOKEN"
-cargo publish --dry-run
+# Uncomment for balenaCI, for now, we will do it manually
+# cargo publish
 
 # repo.yml.type == rust-crate-wasm
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 "$DIR/wasm-build.sh"
+# NPM_USER, _PASS, _EMAIL as Travis CI variables
 npm-cli-login
-npm publish --access public pkg --dry-run
+# Uncomment for balenaCI, for now, we will do it manually
+# npm publish --access public pkg
