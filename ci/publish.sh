6822270bc8b21381d7152548594363d9511140a6
Cyryl Płotnicki <cyryl@balena.io>
2018-11-23 12:42:22 +0000

    make the publish script publish for real

--- ci/publish.sh
+++ ci/publish.sh
@@ -10,13 +10,13 @@ source $HOME/.cargo/env
 echo "Authenticating to cargo..."
 # repo.yml.type == rust-* (rust-crate, rust-crate-wasm)
 cargo login "$CARGO_API_TOKEN"
-echo "Publishing to crates.io"
-# cargo publish
+echo "Publishing to crates.io..."
+cargo publish
 
 # repo.yml.type == rust-crate-wasm
 echo "Authenticating to npm..."
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
 echo "Publishing to npm..."
-# npm publish --access public pkg
+npm publish --access public pkg/unified
 
 echo "Done publishing to crates.io and npm"
