c8e410b71d41ceb45d24632a53b35ec1634ce353
Lucian <lucian.buzzo@gmail.com>
2018-11-26 11:26:42 +0000

    Make browser example interactive
    Change-type: patch
    Signed-off-by: Lucian <lucian.buzzo@gmail.com>

--- examples/browser/index.js
+++ examples/browser/index.js
@@ -1,10 +1,29 @@
 import * as bt from "balena-temen";
 
-console.log(
-    bt.evaluate({
-        "ssid": "Some Cool SSID Network!",
-        "id": {
-            "$$eval": "super.ssid | slugify"
-        }
-    })
-);
+const initialValue = {
+    "ssid": "Some Cool SSID Network!",
+    "id": {
+        "$$eval": "super.ssid | slugify"
+    }
+}
+const stringify = (value) => JSON.stringify(value, null, 2)
+const parse = (value) => JSON.parse(value)
+const $source = document.getElementById('source')
+const $result = document.getElementById('result')
+
+$source.value = stringify(initialValue)
+
+const evaluate = () => {
+    try {
+        const value = parse($source.value)
+        const result = bt.evaluate(value)
+        console.log('evaluation results', $source.value)
+        $result.innerText = stringify(result)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+evaluate()
+
+$source.addEventListener('input', evaluate, false)
