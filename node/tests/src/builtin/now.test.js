751ca66b6e3d72bbb6eaa69cedcccae4aa37c3b9
Robert Vojta <robert@balena.io>
2018-11-27 11:43:08 +0100

    Add temporary fix for the now() fn (wasm)
    Change-type: patch
    Signed-off-by: Robert Vojta <robert@balena.io>

---
+++ node/tests/src/builtin/now.test.js
@@ -0,0 +1,29 @@
+const bt = require('balena-temen');
+
+test('now() generates timestamp', () => {
+    const result = {
+        timestamp: expect.any(Number)
+    };
+
+    expect(
+        bt.evaluate({
+            "timestamp": {
+                "$$eval": "now(timestamp=true)"
+            }
+        })
+    ).toMatchObject(result);
+});
+
+test('now() generates rfc 3339', () => {
+    const result = {
+        date: expect.stringMatching(/^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}\+[0-9]{2}:[0-9]{2}$/)
+    };
+
+    expect(
+        bt.evaluate({
+            "date": {
+                "$$eval": "now()"
+            }
+        })
+    ).toMatchObject(result);
+});
