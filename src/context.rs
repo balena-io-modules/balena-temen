a16933e9f5cc5ed96ecf508eaa3da7ee027bd101
Robert Vojta <robert@balena.io>
2018-11-27 15:02:14 +0100

    Fix now() for wasm
    Change-type: patch
    Signed-off-by: Robert Vojta <robert@balena.io>

--- src/context.rs
+++ src/context.rs
@@ -1,4 +1,22 @@
 use chrono::{DateTime, Utc};
+#[cfg(target_arch = "wasm32")]
+use chrono::NaiveDateTime;
+
+#[cfg(not(target_arch = "wasm32"))]
+fn utc_now() -> DateTime<Utc> {
+    Utc::now()
+}
+
+// chrono crate doesn't support wasm32 arch yet, workaround
+#[cfg(target_arch = "wasm32")]
+fn utc_now() -> DateTime<Utc> {
+    let now = js_sys::Date::new_0();
+    let millisecs_since_unix_epoch: u64 = now.get_time() as u64;
+    let secs = millisecs_since_unix_epoch / 1000;
+    let nanos = 1_000_000 * (millisecs_since_unix_epoch - 1000 * secs);
+    let naive = NaiveDateTime::from_timestamp(secs as i64, nanos as u32);
+    DateTime::from_utc(naive, Utc)
+}
 
 /// An evaluation context
 ///
@@ -21,7 +39,7 @@ impl Context {
             return x;
         }
 
-        let x = Utc::now();
+        let x = utc_now();
         self.cached_now = Some(x);
         x
     }
