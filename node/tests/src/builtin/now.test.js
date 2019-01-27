fc7d3863fde72b04692538a633f72bda60602ffc
Robert Vojta <robert@balena.io>
2019-01-27 17:28:28 +0100

    NOW() accepts one boolean argument only
    Change-type: minor
    Signed-off-by: Robert Vojta <robert@balena.io>

--- node/tests/src/builtin/now.test.js
+++ node/tests/src/builtin/now.test.js
@@ -1,6 +1,6 @@
 const bt = require('balena-temen');
 
-test('NOW() generates timestamp', () => {
+test('NOW(true) generates timestamp', () => {
     const result = {
         timestamp: expect.any(Number)
     };
@@ -8,21 +8,25 @@ test('NOW() generates timestamp', () => {
     expect(
         bt.evaluate({
             "timestamp": {
-                "$$formula": "NOW(true, true)"
+                "$$formula": "NOW(true)"
             }
         })
     ).toMatchObject(result);
 });
 
-test('NOW() generates rfc 3339', () => {
+test('NOW() & NOW(false) generate rfc 3339', () => {
     const result = {
-        date: expect.stringMatching(/^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}\+[0-9]{2}:[0-9]{2}$/)
+        rfc3339implicit: expect.stringMatching(/^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}\+[0-9]{2}:[0-9]{2}$/),
+        rfc3339explicit: expect.stringMatching(/^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}\+[0-9]{2}:[0-9]{2}$/)
     };
 
     expect(
         bt.evaluate({
-            "date": {
+            "rfc3339implicit": {
                 "$$formula": "NOW()"
+            },
+            "rfc3339explicit": {
+                "$$formula": "NOW(false)"
             }
         })
     ).toMatchObject(result);
--- src/builtin/function/now.rs
+++ src/builtin/function/now.rs
@@ -1,35 +1,24 @@
-use chrono::{DateTime, Local, Utc};
+use chrono::{DateTime, Utc};
 use serde_json::{Number, Value};
 
 use crate::context::Context;
 use crate::error::*;
 
 fn now_with_cached(cached: DateTime<Utc>, args: &[Value]) -> Result<Value> {
-    let utc = args.get(0).unwrap_or_else(|| &Value::Bool(false));
-
-    let utc = utc.as_bool().ok_or_else(|| {
-        Error::with_message("invalid argument type")
-            .context("function", "NOW")
-            .context("argument", "1st")
-            .context("argument value", utc.to_string())
-            .context("expected", "boolean")
-    })?;
-
-    let timestamp = args.get(1).unwrap_or_else(|| &Value::Bool(false));
+    let timestamp = args.get(0).unwrap_or_else(|| &Value::Bool(false));
 
     let timestamp = timestamp.as_bool().ok_or_else(|| {
         Error::with_message("invalid argument type")
             .context("function", "NOW")
-            .context("argument", "2nd")
+            .context("argument", "1st")
             .context("argument value", timestamp.to_string())
             .context("expected", "boolean")
     })?;
 
-    match (utc, timestamp) {
-        (true, true) => Ok(Value::Number(Number::from(cached.timestamp()))),
-        (true, false) => Ok(Value::String(cached.to_rfc3339())),
-        (false, true) => Ok(Value::Number(Number::from(cached.with_timezone(&Local).timestamp()))),
-        (false, false) => Ok(Value::String(cached.with_timezone(&Local).to_rfc3339())),
+    if timestamp {
+        Ok(Value::Number(Number::from(cached.timestamp())))
+    } else {
+        Ok(Value::String(cached.to_rfc3339()))
     }
 }
 
@@ -39,7 +28,7 @@ pub(crate) fn now(args: &[Value], context: &mut Context) -> Result<Value> {
 
 #[cfg(test)]
 mod tests {
-    use chrono::{DateTime, Local, NaiveDateTime, Utc};
+    use chrono::{DateTime, NaiveDateTime, Utc};
     use serde_json::json;
 
     use crate::context::Context;
@@ -55,62 +44,29 @@ mod tests {
     #[test]
     fn default_argument_values() {
         let mut ctx = Context::default();
-        let empty_args = vec![];
-        let args = vec![json!(false), json!(false)];
-
-        assert_eq!(now(&empty_args, &mut ctx).unwrap(), now(&args, &mut ctx).unwrap());
+        let args = vec![json!(false)];
+        assert_eq!(now(&[], &mut ctx).unwrap(), now(&args, &mut ctx).unwrap());
     }
 
     #[test]
-    fn local_rfc3339_as_default() {
+    fn utc_rfc3339_as_default() {
         let dt = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(1_541_485_381, 0), Utc);
-        assert_eq!(
-            now_with_cached(dt, &[]).unwrap(),
-            json!(dt.with_timezone(&Local).to_rfc3339())
-        );
-    }
-
-    #[test]
-    fn utc_argument() {
-        let args = vec![json!(true)];
-
-        let dt = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(1_541_485_381, 0), Utc);
-        assert_eq!(now_with_cached(dt, &args).unwrap(), json!(dt.to_rfc3339()));
-        assert_eq!(now_with_cached(dt, &args).unwrap(), json!("2018-11-06T06:23:01+00:00"));
+        assert_eq!(now_with_cached(dt, &[]).unwrap(), json!(dt.to_rfc3339()));
+        assert_eq!(now_with_cached(dt, &[]).unwrap(), json!("2018-11-06T06:23:01+00:00"));
     }
 
     #[test]
     fn timestamp_argument() {
-        let args = vec![json!(true), json!(true)];
+        let args = vec![json!(true)];
 
         let dt = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(1_541_485_381, 0), Utc);
         assert_eq!(now_with_cached(dt, &args).unwrap(), json!(1_541_485_381));
-    }
-
-    #[test]
-    fn utc_argument_does_not_modify_timestamp() {
-        let args = vec![json!(false), json!(true)];
-
-        let dt = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(1_541_485_381, 0), Utc);
-        assert_eq!(now_with_cached(dt, &args).unwrap(), json!(1_541_485_381));
-
-        let args = vec![json!(true), json!(true)];
-        assert_eq!(now_with_cached(dt, &args).unwrap(), json!(1_541_485_381));
-    }
-
-    #[test]
-    fn fail_on_invalid_utc_argument_type() {
-        let mut ctx = Context::default();
-        let args = vec![json!(1)];
-
-        assert!(now(&args, &mut ctx).is_err());
     }
 
     #[test]
     fn fail_on_invalid_timestamp_argument_type() {
         let mut ctx = Context::default();
-        let args = vec![json!(true), json!("foo")];
-
+        let args = vec![json!("foo")];
         assert!(now(&args, &mut ctx).is_err());
     }
 }
